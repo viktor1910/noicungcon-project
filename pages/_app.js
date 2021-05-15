a94e8b94fc60a7bbc573db8ee0103a76d924fc73
viktor <viktor@1on1english.vn>
2021-05-15 15:26:36 +0700

    init project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const theme = {};
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
