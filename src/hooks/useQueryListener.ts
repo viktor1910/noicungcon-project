73a5b06a25422115ece7bcd1df57692233e94f5c
viktor <viktor.nguyen@transwap.com>
2021-05-22 21:07:15 +0700

    config linter

--- src/hooks/useQueryListener.ts
+++ src/hooks/useQueryListener.ts
@@ -1,21 +1,20 @@
-import { useEffect, useState } from "react";
-import { useIsFetching, useQueryClient } from "react-query";
-import { QueryFilters } from 'react-query/types/core/utils'
-import _isEqual from 'lodash/isEqual'
-
+import { useEffect, useState } from 'react';
+import { useIsFetching, useQueryClient } from 'react-query';
+import { QueryFilters } from 'react-query/types/core/utils';
+import _isEqual from 'lodash/isEqual';
 
 const useQueryListener = <T>(queryKey: string, filters?: QueryFilters) => {
-    const [data, setData] = useState<T>()
-    const queryClient = useQueryClient()
-    const numberOfFetching = useIsFetching(queryKey, filters)
-    useEffect(() => {
-        if (!numberOfFetching) {
-            const newData = queryClient.getQueryData<T>(queryKey)
-            if (!_isEqual(data, newData)) setData(newData)
-        }
-    }, [numberOfFetching, queryClient, data, queryKey])
+  const [data, setData] = useState<T>();
+  const queryClient = useQueryClient();
+  const numberOfFetching = useIsFetching(queryKey, filters);
+  useEffect(() => {
+    if (!numberOfFetching) {
+      const newData = queryClient.getQueryData<T>(queryKey);
+      if (!_isEqual(data, newData)) setData(newData);
+    }
+  }, [numberOfFetching, queryClient, data, queryKey]);
 
-    return data
-}
+  return data;
+};
 
-export default useQueryListener
+export default useQueryListener;
